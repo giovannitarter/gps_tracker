06bbcd76d88ad93e199c5d68efddb1146b96b2ae
Giovanni Tarter <giovanni.tarter@gmail.com>
2024-02-11 16:53:45 +0100

    modified:   platformio.ini 	modified:   src/LoRaSenderESP32.ino

--- localizer/src/LoRaSenderESP32.ino
+++ localizer/src/LoRaSenderESP32.ino
@@ -1,18 +1,46 @@
 #include <SPI.h>
 #include <LoRa.h>
-#include <TinyGPS.h>
+#include <TinyGPSPlus.h>
 
 int counter = 0;
 
-TinyGPS gps;
+TinyGPSPlus gps;
+
+
+void writeInfo(char *out, size_t len)
+{
+
+  char location[30];
+  if (gps.location.isValid()) {
+    snprintf(location, 30, "%.6f %.6f", gps.location.lat(), gps.location.lng());
+  }
+  else {
+    strcpy(location, "INVALID");
+  }
+
+  snprintf(out, len,
+    "Location: %s, Date/Time: %02d/%02d/%04d %02d:%02d:%02d.%02d",
+    location,
+    gps.date.day(),
+    gps.date.month(),
+    gps.date.year(),
+    gps.time.hour(),
+    gps.time.minute(),
+    gps.time.second(),
+    gps.time.centisecond()
+    );
+
+}
 
 void setup() {
 
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial);
 
   Serial1.begin(9600, SERIAL_8N1, 3, 2);
-  while (!Serial1);
+  //while (!Serial1);
+
+  delay(1000);
 
   LoRa.setPins(7, 10, 1); // set CS, reset, IRQ pin
 
@@ -36,47 +64,21 @@ void loop() {
   bool newData = false;
   unsigned long chars;
   unsigned short sentences, failed;
+  char position[100];
 
   while (Serial1.available() > 0) {
     if (gps.encode(Serial1.read())) {
-      newData = true;
     }
   }
 
-  if (newData)
-  {
-    float flat, flon;
-    unsigned long age;
-    gps.f_get_position(&flat, &flon, &age);
-    Serial.print("LAT=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" SAT=");
-    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    Serial.print(" PREC=");
-    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
-    Serial.print(' ');
-  }
-
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print("CHARS=");
-  Serial.print(chars);
-  Serial.print(" SENTENCES=");
-  Serial.print(sentences);
-  Serial.print(" CSUM ERR=");
-  Serial.println(failed);
-  if (chars == 0) {
-    Serial.println("** No characters received from GPS: check wiring **");
-  }
-
+  writeInfo(position, 100);
+  Serial.print(position);
 
   Serial.printf("Sending packet: %d\n\r", counter);
 
   // send packet
   LoRa.beginPacket();
-  LoRa.printf("% 10d - hello %d", millis(), counter);
-  //LoRa.print(counter);
+  LoRa.printf("% 10d - %s %d\n\r", millis(), position, counter);
   LoRa.endPacket();
 
   counter++;
