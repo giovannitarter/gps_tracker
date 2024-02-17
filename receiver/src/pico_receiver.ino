cd976c0db40ae12853d3e6d640310a12f3b90df1
Giovanni Tarter <giovanni.tarter@gmail.com>
2024-02-17 08:55:59 +0100

    new file:   src/LoRa-RP2040.cpp 	new file:   src/LoRa-RP2040.h 	new file:   src/pico_receiver.ino

---
+++ receiver/src/pico_receiver.ino
@@ -0,0 +1,57 @@
+/*
+  LoRa register dump
+
+  This examples shows how to inspect and output the LoRa radio's
+  registers on the Serial interface
+*/
+#include <SPI.h>              // include libraries
+#include "LoRa-RP2040.h"
+
+void setup() {
+
+  delay(2000);
+  
+  Serial.begin(115200);               // initialize serial
+  while (!Serial);
+
+    LoRa.setPins(1, 4, 5); // set CS, reset, IRQ pin
+
+  Serial.println("LoRa Dump Registers");
+
+  if (!LoRa.begin(434E6)) {         // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                   // if failed, do nothing
+  }
+
+  LoRa.dumpRegisters(Serial);
+  
+}
+
+
+void loop() {
+
+
+  int packetSize = 0;
+
+  Serial.println("\nListening");
+  while (true) {
+    packetSize = LoRa.parsePacket();
+    if (packetSize) {
+      break;
+    }
+  }
+    
+  if (packetSize) {
+  // received a packet
+  Serial.print("Received packet '");
+
+  // read packet
+  while (LoRa.available()) {
+    Serial.print((char)LoRa.read());
+  }
+
+  // print RSSI of packet
+  Serial.print("' with RSSI ");
+  Serial.println(LoRa.packetRssi());  
+  }
+}
